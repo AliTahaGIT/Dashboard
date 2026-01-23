72ef3deed064c110669eb170a1478789988289bd
AliTahaGIT <aliahmedtaha7077@gmail.com>
2026-01-24 02:20:36 +0400

    Oops forgot to add User Avatar :))

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,17 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className='nav-main'>
-        <h1>Welcome Admin</h1>
-        <div className="nav-icon">
-           <img src="https://api.iconify.design/mdi:bell-outline.svg?color=%23F7F6F2" alt="notifications" className="nav-img"/>
-           <span className="noti-badge">12</span>
+        <div className='nav-left'>
+            <h1>Welcome Admin</h1>
+            <div className="nav-icon">
+                <img src="https://api.iconify.design/mdi:bell-outline.svg?color=%23F7F6F2" alt="notifications" className="nav-img"/>
+                <span className="noti-badge">12</span>
+            </div>
+        </div>
+
+        <div className='nav-pfp'>
+            <img alt='pfp' src='https://api.iconify.design/mdi:account-circle.svg?color=%23F7F6F2' className='pfp-img'/>
+            <p className='pfp-arrow'>⌄</p>
         </div>
     </div>
   )
